a2e0f275595225985eee6b698057a8b6f628e61f
Kiljune Choi <kiljune@gmail.com>
2025-04-21 20:50:28 +0900

    no root passwd

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo nixos-generate-config --no-filesystems --root /mnt
 sudo cp *.nix /mnt/etc/nixos
 sudo cp -r /mnt/etc /mnt/persistent
 
-sudo nixos-install --root /mnt --flake /mnt/etc/nixos#default
+sudo nixos-install --no-root-passwd --root /mnt --flake /mnt/etc/nixos#default
