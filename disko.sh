98d75f6ff6fb2438b6676ff7c9abb0cd28f107f6
Kiljune Choi <36096031+kiljune@users.noreply.github.com>
2025-05-27 13:12:30 +0900

    Update disko.sh

--- disko.sh
+++ disko.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount disko.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount disko.nix --arg device '"/dev/disk/by-id/ata-2.5__SSD_512GB_CL2025022400573K"'
 
 lsblk
